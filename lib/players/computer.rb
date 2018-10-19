48efc72e9ff9482efc9af1ad971ffa0d90f624cc
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-19 13:28:47 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -57,6 +57,7 @@ module Players
       opponent_token = self.token == "X" ? "O" : "X"
       Game.win_combos.find { |combo|
         tokens = combo.map {|spot| token_at(spot)}
+        binding.pry
         tokens.count { |s| s == token } == 2 || tokens.count { |s| s == opponent_token } == 2
         }
     end
