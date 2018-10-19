94491129bdabde92c11d6762e954f6249bd86ff9
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-19 13:43:16 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -50,16 +50,6 @@ module Players
       "#{integer_move(board)+1}"
     end # end #move(board)
 
-=begin (this is the way to do multi-line comments in ruby. =begin and =end CANNOT BE INDENTED)
-    NOTE: I'm pretty damn sure these can all be refactored using ternary operator.
-    Something like return !taken?(MIDDLE) ? MIDDLE : possible_any_win : free_corner : first_empty_spot
-    Okay, actually, I'm pretty sure that doesn't work and that it would have to be more like:
-    !taken?(MIDDLE) ? MIDDLE :
-      possible_any_win ? possible_any_win :
-      free_corner ? free_corner :
-      first_empty_spot
-    So maybe it's not all that great.
-=end
 
     def integer_move(board) # => 0-based board index
       @board = board
@@ -69,23 +59,12 @@ module Players
         free_corner ? free_corner :
         first_empty_spot
 
-      # if !taken?(MIDDLE)
-      #   MIDDLE
-      # elsif possible_any_win
-      #   # binding.pry
-      #   open_spot(possible_any_win)
-      # elsif free_corner
-      #   # binding.pry
-      #   free_corner
-      # else
-      #   # binding.pry
-      #   first_empty_spot
-      # end # end if
     end # end #integer_move(board)
 
   end # end class
 end # end module
 
+# CODE ARCHIVE
 =begin
   METHOD ARCHIVE
 
@@ -118,5 +97,17 @@ end # end module
     end
   end
 
+  # if !taken?(MIDDLE)
+  #   MIDDLE
+  # elsif possible_any_win
+  #   # binding.pry
+  #   open_spot(possible_any_win)
+  # elsif free_corner
+  #   # binding.pry
+  #   free_corner
+  # else
+  #   # binding.pry
+  #   first_empty_spot
+  # end # end if
 
 =end
